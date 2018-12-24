5404689db087cd6df2d7d258b81e56602ffacaf2
Austin Mayes <austin@avicus.net>
2018-12-24 15:23:14 -0600

    Improve gone fishing

--- PGM/src/main/java/tc/oc/pgm/mutation/types/uhc/GoneFishingScenario.java
+++ PGM/src/main/java/tc/oc/pgm/mutation/types/uhc/GoneFishingScenario.java
@@ -2,11 +2,14 @@ package tc.oc.pgm.mutation.types.uhc;
 
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import tc.oc.pgm.match.Match;
 import tc.oc.pgm.mutation.Mutation;
 import tc.oc.pgm.mutation.types.UHCMutation;
+import tc.oc.pgm.spawns.events.ParticipantReleaseEvent;
 
 public class GoneFishingScenario extends UHCMutation.Impl {
 
@@ -20,8 +23,14 @@ public class GoneFishingScenario extends UHCMutation.Impl {
         ItemMeta meta = rod.getItemMeta();
         meta.addEnchant(Enchantment.DURABILITY, 250, true);
         meta.addEnchant(Enchantment.LUCK, 250, true);
+        meta.addEnchant(Enchantment.LURE, 250, true);
         rod.setItemMeta(meta);
-        return new ItemStack[]{rod};
+        return new ItemStack[]{rod, new ItemStack(Material.ANVIL, 20)};
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onBegin(ParticipantReleaseEvent event) {
+        event.getPlayer().getBukkit().setLevel(2000);
     }
 
     @Override
